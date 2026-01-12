0151ff31a5b9d2440ac7494e860b83b78d97a924
gamo <gamothegod@gmail.com>
2026-01-12 15:59:47 +0300

    Added Post Processing elements and Oscillating Obstacle

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float speed;
+    Vector3 startPosition;
+    Vector3 endPosition;
+    float movementFactor; // 0 for start, 1 for end
+
+
+    void Start()
+    {
+        startPosition = transform.position;
+        endPosition = startPosition + movementVector;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementFactor = Mathf.PingPong(Time.time * speed, 1f);
+        transform.position = Vector3.Lerp(startPosition, endPosition, movementFactor);
+    }
+}
